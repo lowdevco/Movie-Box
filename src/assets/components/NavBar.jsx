d56120fb5239721c22d82d9024e169ba5f30e00a
Irfan <muhammadirfank2007@gmail.com>
2026-03-05 21:35:32 +0530

    Home Respnsive issue for 760px Fixed

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -6,10 +6,12 @@ function NavBar() {
   return (
     <nav className="navbar">
       <div className="navbar-brand">
-        <Link to="/">🎬 MovieBox</Link>
+        <Link to="/">
+          <span className="nav-mobile">🎬</span> MovieBox
+        </Link>
       </div>
       <div className="navbar-links">
-        <Link to="/" className="nav-link">
+        <Link to="/" className="nav-link" id="home-mobile">
           Home
         </Link>
         <Link to="/favorites" className="nav-link">
